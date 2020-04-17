4685ae773e5e14f25da5ee3e961b434ad510e399
asal <asal.react@gmail.com>
2020-04-17 10:25:00 -0400

    changed toolbar code by using mdbreact package

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,33 +1,81 @@
-import React from 'react';
-import { Navbar, Nav, } from 'react-bootstrap';
-import classes from './Toolbar.css';
+import React, {Component} from 'react';
+import styles from './Toolbar.module.css';
 import logo from '../../../assets/Images/logo2.png';
+import image from '../../../assets/Images/bg1.png';
+import {MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBNavLink, MDBContainer, MDBMask, MDBView } from "mdbreact";
 
-const toolbar = (props) => (
-   <>
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
-    <Navbar.Brand href="#home">
-      <img
-        alt=""
-        src={logo}
-        width="200"
-        height="60"
-        className="d-inline-block align-top"
-      />{' '}
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    <Navbar.Collapse id="responsive-navbar-nav">
-         <Nav className="ml-auto" activeKey="/home">
-            <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="#">About us</Nav.Link>
-            <Nav.Link href="#">My Items</Nav.Link>
-            <Nav.Link href="#">Logout</Nav.Link>
-            <Nav.Link href="#">LogIn</Nav.Link>
-        </Nav>
-    </Navbar.Collapse>
-    </Navbar>
+class Toolbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapse: false,
+      isWideEnough: false
+    };
+    this.onClick = this.onClick.bind(this);
+  }
 
-   </>
-);
+  onClick() {
+    this.setState({
+      collapse: !this.state.collapse
+    });
+  }
 
-export default toolbar;
+    render(){
+      return(
+        <div>
+          <header>
+              <MDBNavbar color="bg-primary" dark expand="md" fixed="top" scrolling transparent>
+              <MDBNavbarBrand href="/">
+                <img
+                  alt=""
+                  src={logo}
+                  width="200"
+                  height="60"
+                  className="d-inline-block align-top"
+                />{' '}
+              </MDBNavbarBrand>
+              {!this.state.isWideEnough && <MDBNavbarToggler onClick={this.onClick} />}
+              <MDBCollapse id="navbarCollapse3" isOpen={this.state.collapse} navbar>
+              <MDBNavbarNav right>
+                <MDBNavItem active>
+                  <MDBNavLink to="#!">Home</MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                  <MDBNavLink to="#!">About Us</MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                  <MDBNavLink to="#!">My Items</MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                  <MDBNavLink to="#!">LogIn</MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                  <MDBNavLink to="#!">Logout</MDBNavLink>
+                </MDBNavItem>
+        
+              </MDBNavbarNav>
+              
+            </MDBCollapse>
+            </MDBNavbar>
+            <MDBView className={styles.view}>
+            <img 
+                src={image}
+                className="img-fluid"
+                alt=""
+              />
+              <MDBMask overlay="black-light" className={`text-white text-center ${styles.caption}`}>
+                <h2>This is Barter</h2>
+                <h5>It will always be a place that you can barter</h5>
+                <br />
+                {/* <p>Full page intro with background image will be always displayed in full screen mode, regardless of device </p> */}
+              </MDBMask>
+          </MDBView>
+          </header>
+        </div>
+       
+      );
+    }
+}
+
+
+export default Toolbar;
