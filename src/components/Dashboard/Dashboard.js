b1d99290509fecd5d33db0273e896b7594059cbb
asal <asal.react@gmail.com>
2020-05-25 14:31:27 -0400

    added new item button

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import 
 
 const dashboard = (props) => {
+  const  addItemHandler = () => {
+        props.history.push('/item');
+    }
     return(
         <div>
             <h1>Dashboard</h1>
+            <button onClick={addItemHandler}>Add New Item</button>
         </div>
     )
 };
