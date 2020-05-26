9bfed78bb00a6c6497ed6cbf39ce0f82fab7e9ec
asal <asal.react@gmail.com>
2020-05-26 16:28:25 -0400

    added media quary

--- src/components/Share/Button/Button.js
+++ src/components/Share/Button/Button.js
@@ -4,8 +4,7 @@ import {Button} from 'react-bootstrap';
 
 const button = (props) => {
     return (
-    //    <button className={styles.button}  onClick={props.clicked}>{props.title}</button>
-       <Button variant="warning" size="md" onClick={props.clicked}>{props.title}</Button>
+       <Button variant="warning" size="md"  onClick={props.clicked}>{props.title}</Button>
     );   
 }
 
