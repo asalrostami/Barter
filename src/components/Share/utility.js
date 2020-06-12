0477ac1657907a7a0d0131ee3d189ea181db0be5
asal <asal.react@gmail.com>
2020-06-12 16:27:17 -0400

    modified some changes

--- src/components/Share/utility.js
+++ src/components/Share/utility.js
@@ -5,21 +5,21 @@ export const updateObject = (oldObject, updatedProperties) => {
     };
 };
 export const checkValidity = (value, rules) => {
-    let isValid = false;
+    let isValid = true;
     
     if(!rules) {
         return true;
     }
-    if (rules == "required") {
+    if (rules === "required") {
         isValid = value.trim() !== '' && isValid;
     }
 
-    if (rules == "isEmail") {
+    if (rules === "isEmail") {
         const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
         isValid = pattern.test(value) && isValid
     }
 
-    if (rules == "isNumeric") {
+    if (rules === "isNumeric") {
         const pattern = /^\d+$/;
         isValid = pattern.test(value) && isValid
     }
