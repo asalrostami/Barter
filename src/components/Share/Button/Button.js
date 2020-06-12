a277165066fee880b4158f3856347f2a430dbc7d
asal <asal.react@gmail.com>
2020-06-12 16:27:34 -0400

    added more attributes

--- src/components/Share/Button/Button.js
+++ src/components/Share/Button/Button.js
@@ -4,7 +4,7 @@ import {Button} from 'react-bootstrap';
 
 const button = (props) => {
     return (
-       <Button variant="warning" size="md"  onClick={props.clicked}>{props.title}</Button>
+       <Button variant="warning" size="md" type={props.type} onClick={props.clicked} disabled={props.disabled}>{props.title}</Button>
     );   
 }
 
