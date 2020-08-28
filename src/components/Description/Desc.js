4d0e6060b1bdb2dd170ce8d2bcd95e8063dfb539
asal <asal.react@gmail.com>
2020-08-28 16:56:04 -0400

    designed the description page

--- src/components/Description/Desc.js
+++ src/components/Description/Desc.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
+import styles from './Desc.module.css';
+import pic from '../../assets/Images/trade1.jpg';
 
 class Desc extends Component  {
     render(){
         return(
-            <div>
-                <h1>Description</h1>
-    
+            <div className={styles.desc}>
+                <img src={pic}  className={styles.image}/>
+                <div className={styles.text}>
+                   <h1>Your request has been approved</h1>
+                </div>
             </div>
         )
     }
